6236998cd092544f71502245052511117e408d74
MikeTeddyOmondi <mike_omondi@outlook.com>
2022-09-13 16:07:03 +0300

    Updated sections stale information

--- src/_data/siteInfo.js
+++ src/_data/siteInfo.js
@@ -2,8 +2,12 @@
 
 module.exports = {
 	assets: "./assets",
-	favicon: "./assets/img/favicon-BITCOIN.ico",
-	brandImage: "./assets/img/Bitcoin_BRAND_LOGO.png",
 	author: "Mike Teddy Omondi",
+	email: "mike_omondi@outlook.com",
+	dateOfBirth: "February 13th",
+	website: "www.miketeddyomondi.dev",
 	copyrightYear: new Date().getFullYear(),
+	favicon: "./assets/img/favicon-BITCOIN.ico",
+	jobs: "Software Development & Ethical Hacking",
+	brandImage: "./assets/img/Bitcoin_BRAND_LOGO.png",
 };
