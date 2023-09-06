5bb974cc47062df5283d4ae4e800c9fbc70becfe
MikeTeddyOmondi <mike_omondi@outlook.com>
2023-09-06 14:47:56 +0300

    Updated - src/js/plugin.js file for Google script

--- src/js/plugins.js
+++ src/js/plugins.js
@@ -5,33 +5,28 @@
  * ------------------------------------------------------------------- */
 //
 
+// https://script.google.com/macros/s/AKfycbwx6rxe5Q6Xeuw7uV0TZwMFZqgNBqi6rJAxLqz6TZKzEM1zJepjAkZZ6LubzH4ijq4/exec
+
 function saveToGoogleSheet() {
-	var email = document.getElementById("mc-email").value;
-	var url =
-		"https://script.google.com/macros/s/AKfycbwlJd66uBen8fjpIvf7UqMpDoMElboI6_ZDTZOZQkRhYCq80JxhTcAJEXweVWQoN7Pf/exec";
-	// AKfycbwlJd66uBen8fjpIvf7UqMpDoMElboI6_ZDTZOZQkRhYCq80JxhTcAJEXweVWQoN7Pf
-	// https://script.google.com/macros/s/AKfycbwlJd66uBen8fjpIvf7UqMpDoMElboI6_ZDTZOZQkRhYCq80JxhTcAJEXweVWQoN7Pf/exec
+  var email = document.getElementById("mc-email").value;
+  var url =
+    "https://script.google.com/macros/s/AKfycbwx6rxe5Q6Xeuw7uV0TZwMFZqgNBqi6rJAxLqz6TZKzEM1zJepjAkZZ6LubzH4ijq4/exec";
 
-	// var request = new XMLHttpRequest();
-	// request.open("POST", url, true);
-	// request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-	// request.send("email=" + email);
+  async function postData(data) {
+    try {
+      const response = await fetch(url, {
+        method: "POST",
+        body: data,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      console.log({ response });
+    } catch (error) {
+      console.log({ error });
+    }
+  }
 
-	async function postData(data) {
-		try {
-			const response = await fetch(url, {
-				method: "POST",
-				body: data,
-				headers: {
-					"Content-Type": "text/plain;charset=utf-8",
-				},
-			});
-			console.log({ response });
-		} catch (error) {
-			console.log({ error });
-		}
-	}
-
-	postData(email);
-	document.getElementById("mc-email").value = "";
+  postData(email);
+  document.getElementById("mc-email").value = "";
 }
